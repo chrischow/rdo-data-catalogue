241f4c8b8a2806277a935ea4863ebb452d3201fc
Chris Chow <christian.chow.en.yi@gmail.com>
2022-06-14 21:12:02 +0800

    Add functions for getting items and performing joins

--- data-catalogue/dev/src/utils/processData.js
+++ data-catalogue/dev/src/utils/processData.js
@@ -26,3 +26,35 @@ export function processMetadata(tables, metadataCallback, tableCallback) {
     tableCallback(tables);
   }
 };
+
+// Join functions
+export function leftJoin(left, right, left_on, right_on) {
+  const data = left.map(leftRow => ({
+    ...right.find(rightRow => leftRow[left_on] === rightRow[right_on]),
+    ...leftRow
+  }));
+  return data;
+}
+
+export function innerJoin(left, right, left_on, right_on) {
+  // Get matching keys
+  const leftFk = left.map(row => row[left_on]);
+  const rightPk = right.map(row => row[right_on]);
+  const commonKeys = leftFk.filter(value => rightPk.includes(value));
+
+  const leftFiltered = left.filter(row => commonKeys.includes(row[left_on]));
+  let output = [];
+
+  leftFiltered.forEach(leftRow => {
+    right.forEach(rightRow => {
+      if (leftRow[left_on] === rightRow[right_on]) {
+        output.push({
+          ...rightRow,
+          ...leftRow
+        });
+      }
+    });
+  });
+
+  return output;
+}
--- data-catalogue/dev/src/utils/queryData.js
+++ data-catalogue/dev/src/utils/queryData.js
@@ -3,6 +3,37 @@ import $ from 'jquery';
 // Configs
 const apiUrl = 'http://127.0.0.1:5000/ravenpoint/_api/';
 
+// Generic query
+export function getListItems(listId, selectStr, filterStr, expandStr, callback) {
+  const output = $.ajax({
+    url: `${apiUrl}web/Lists(guid'${listId}')/items?` +
+      `$select=${selectStr}` +
+      `${filterStr ? '&$filter=' + filterStr : ''}` +
+      `${expandStr ? '&$expand=' + expandStr : ''}`,
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json; odata=nometadata'
+    },
+    async: true,
+    success: data => {
+      const expandedData = [...data.value];
+      const expandColumns = expandStr.split(',');
+      if (expandColumns[0]) {
+        expandColumns.forEach(column => {
+          for (let i=0; i < expandedData.length; i++) {
+            expandedData[i][`${column}_Id`] = expandedData[i][column].Id;
+            expandedData[i][`${column}_Title`] = expandedData[i][column].Title;
+            delete expandedData[i][column];
+          }
+        });
+      }
+      callback(expandedData);
+    }
+  });
+  
+  return output.value;
+}
+
 // READ
 export function getDatasets(listId, dataDomain, callback) {
 
