16deac7fb79783a2903d6e73f3fc455f37806b21
Chris Chow <christian.chow.en.yi@gmail.com>
2022-06-14 22:04:00 +0800

    Add function to handle change

--- data-catalogue/dev/src/components/SearchBar/SearchBar.js
+++ data-catalogue/dev/src/components/SearchBar/SearchBar.js
@@ -2,9 +2,14 @@ import Form from 'react-bootstrap/Form';
 import './SearchBar.css'
 
 export default function SearchBar(props) {
+  // Function to handle change in keywords
+  const handleChange = (event) => {
+    props.updateSearch(event.target.value);
+  }
+
   return (
     <Form>
-      <Form.Control type="text" placeholder={props.placeholder} className="searchbar--bar mx-auto">
+      <Form.Control type="text" placeholder={props.placeholder} className="searchbar--bar mx-auto" onChange={handleChange}>
       </Form.Control>
     </Form>
   );
