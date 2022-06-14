b661cd6e569ececb4f9af255056c15e59d1200f4
Chris Chow <christian.chow.en.yi@gmail.com>
2022-06-15 00:59:41 +0800

    Remove unused functions

--- data-catalogue/dev/src/utils/processData.js
+++ data-catalogue/dev/src/utils/processData.js
@@ -1,32 +1,3 @@
-// Callback to process tables w/ domains
-export function processMetadata(tables, metadataCallback, tableCallback) {
-  if (tables.length == 0) {
-    return
-  }
-  const newMetadata = {}
-  let currentDataset;
-  for (let i=0; i < tables.length; i++) {
-    currentDataset = tables[i].datasetTitle;
-    if (currentDataset in newMetadata) {
-      newMetadata[currentDataset].nTables ++;
-    } else {
-      newMetadata[currentDataset] = {
-        datasetId: tables[i].datasetId,
-        datasetTitle: tables[i].datasetTitle,
-        useCases: tables[i].datasetUseCases,
-        owner: tables[i].datasetOwner,
-        pointOfContact: tables[i].datasetPoc,
-        dataDomain: tables[i].dataDomain,
-        nTables: 1
-      }
-    }
-  }
-  metadataCallback(newMetadata);
-  if (tableCallback) {
-    tableCallback(tables);
-  }
-};
-
 // Join functions
 export function leftJoin(left, right, left_on, right_on) {
   const data = left.map(leftRow => ({
