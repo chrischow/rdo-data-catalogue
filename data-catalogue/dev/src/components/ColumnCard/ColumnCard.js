cb35b30f0d0500d63df3e52041910f6f5ab5f1f3
Chris Chow <christian.chow.en.yi@gmail.com>
2022-06-16 13:49:36 +0800

    Add column card component

---
+++ data-catalogue/dev/src/components/ColumnCard/ColumnCard.js
@@ -0,0 +1,49 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { BsKeyFill } from 'react-icons/bs';
+import { FiColumns } from 'react-icons/fi';
+import TermPopover from '../TermPopover/TermPopover';
+import './ColumnCard.css';
+
+export default function ColumnCard(props){
+  return (
+    <div className="columncard mt-3 mb-3">
+      <div className="columncard--title-section">
+        <h5 className="columncard--title d-flex align-items-center">
+          <FiColumns style={{ color: '#E6BA0F', marginRight: '10px' }} />
+          {props.Title}
+          {props.isPrimaryKey ?
+            <BsKeyFill alt="Primary Key" style={{ color: '#F0C419', marginLeft: '10px' }} /> :
+          ''}
+          {props.isForeignKey ?
+            <BsKeyFill alt="Foreign Key" style={{ color: '#FF5364', marginLeft: '10px' }} /> :
+          ''}
+          <span className="columncard--tag">{props.dataType}</span>
+        </h5>
+      </div>
+      <div className="columncard--body-section">
+        <Row className="mt-3">
+          <Col xs={8}>
+            <h5 className="columncard--section-header">Definition</h5>
+            <p>{props.columnDescription}</p>
+          </Col>
+          <Col xs={4}>
+            <h5 className="columncard--section-header">Business Terms</h5>
+            {props.termList.length > 0 && props.termList.map(term => {
+              return (
+                <span className="mr-5">
+                  <TermPopover key={`popover-${props.Id}-${term.Id}`} {...term} />
+                  <span> </span>
+                </span>
+              );
+            })}
+          </Col>
+        </Row>
+        <div className="mt-3">
+          <h5 className="columncard--section-header">Business Rules</h5>
+          <p>{props.businessRules}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
