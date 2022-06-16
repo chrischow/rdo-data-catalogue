d113045612b4de67641b303979739c1edded639f
Chris Chow <christian.chow.en.yi@gmail.com>
2022-06-16 18:36:28 +0800

    Amend layout for column card

--- data-catalogue/dev/src/components/ColumnCard/ColumnCard.js
+++ data-catalogue/dev/src/components/ColumnCard/ColumnCard.js
@@ -1,11 +1,21 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Accordion from 'react-bootstrap/Accordion';
 import { BsKeyFill } from 'react-icons/bs';
 import { FiColumns } from 'react-icons/fi';
 import TermPopover from '../TermPopover/TermPopover';
+import DqScoreColumn from '../DqScoreColumn/DqScoreColumn';
 import './ColumnCard.css';
 
 export default function ColumnCard(props){
+  const randomMetrics = () => {
+    return [
+      {metric: 'accuracy', score: Math.random()},
+      {metric: 'completeness', score: Math.random()},
+      {metric: 'consistency', score: Math.random()},
+    ];
+  }
+
   return (
     <div className="columncard mt-3 mb-3">
       <div className="columncard--title-section">
@@ -23,11 +33,17 @@ export default function ColumnCard(props){
       </div>
       <div className="columncard--body-section">
         <Row className="mt-3">
-          <Col xs={8}>
-            <h5 className="columncard--section-header">Definition</h5>
-            <p>{props.columnDescription}</p>
+          <Col xs={9}>
+            <div>
+              <h5 className="columncard--section-header">Definition</h5>
+              <p>{props.columnDescription}</p>
+            </div>
+            <div className="mt-4">
+              <h5 className="columncard--section-header">Business Rules</h5>
+              <p>{props.businessRules}</p>
+            </div>
           </Col>
-          <Col xs={4}>
+          <Col xs={3}>
             <h5 className="columncard--section-header">Business Terms</h5>
             {props.termList.length > 0 && props.termList.map(term => {
               return (
@@ -39,10 +55,20 @@ export default function ColumnCard(props){
             })}
           </Col>
         </Row>
-        <div className="mt-3">
-          <h5 className="columncard--section-header">Business Rules</h5>
-          <p>{props.businessRules}</p>
-        </div>
+        <Accordion flush className="mt-3">
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>
+              <h5 className="columncard--dq-header">Data Quality Scores</h5>
+            </Accordion.Header>
+            <Accordion.Body>
+              <Row className="mt-3">
+                {randomMetrics().map(elem => {
+                  return <DqScoreColumn {...elem} />
+                })}
+              </Row>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </div>
     </div>
   );
